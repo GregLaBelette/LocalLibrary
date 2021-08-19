c911cb1b206999c916233bd1afb6161b9b79411c
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-08-19 14:50:58 +0200

    defined all catalog routes, controllers are dummy functions for the moment"

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
