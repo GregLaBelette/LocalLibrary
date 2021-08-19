d2c71811dcd809048b3173cca34f183a84de0067
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-08-19 16:17:13 +0200

    just before checkout, created test route and checking old index view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var testRouter = require('./toutes/test');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
