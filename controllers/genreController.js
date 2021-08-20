0ff7c72c46e3f4cda952595673ee6b5ef68da21d
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-08-20 12:11:38 +0200

    created genre list controller + view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,16 @@
 var Genre = require('../models/genre');
 
-// Display list of all Genre.
-exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+// Display list of all Genres.
+exports.genre_list = function(req, res, next) {
+    
+    Genre.find()
+    .sort([['name', 'ascending']])
+    .exec(function (err, list_genres) {
+        if (err) {return next(err); }
+        //successful, so render
+        res.render('genre_list', {title: 'Genre List', genre_list: list_genres})
+    })
+
 };
 
 // Display detail page for a specific Genre.
