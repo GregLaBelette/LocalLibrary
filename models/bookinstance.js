32a9a2f8a7ae632af22529335654b921331b9412
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-08-20 10:58:24 +0200

    installed luxon & formatted due date + set locale to en-GB

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const {DateTime} = require('luxon');
 
 var Schema = mongoose.Schema;
 
@@ -16,6 +17,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual for bookinstance's due date formatting
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).setLocale('en-UK').toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
