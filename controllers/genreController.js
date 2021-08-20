c80ea162623e32643901e93075f492203cfa01bc
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-08-20 12:30:16 +0200

    created genre detail controller + view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+
+var async = require('async');
 
 // Display list of all Genres.
 exports.genre_list = function(req, res, next) {
@@ -14,9 +17,32 @@ exports.genre_list = function(req, res, next) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+              .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: results.genre.name, genre: results.genre, genre_books: results.genre_books } );
+    });
+
 };
+
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
