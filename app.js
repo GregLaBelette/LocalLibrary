1871c95ff267142bd9bad35c694e24168688bc36
Grégoire Vallette <gregoire.vallette@gmail.com>
2021-09-16 16:04:36 +0200

    Added files and changes for deployment on Heroku

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ var testRouter = require('./routes/test');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://gregLaBelette:CDauguet90@mdn-express.cpoqm.mongodb.net/local_library?retryWrites=true&w=majority';
+
+var dev_db_url = 'mongodb+srv://gregLaBelette:CDauguet90@mdn-express.cpoqm.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MOGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
